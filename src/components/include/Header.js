47cba943a14f02c5da9795e54735a0ba4a6d41fc
ene8255 <86288109+ene8255@users.noreply.github.com>
2022-03-02 17:47:25 +0900

    포트폴리오 사이트 만들기 시작

---
+++ src/components/include/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Inhye Kim</h1>
+            <nav>
+                <ul>
+                    <li>About me</li>
+                    <li>Skills</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
