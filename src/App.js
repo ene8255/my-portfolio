ce2f695b51d3f643f1ce5b47ed4e140c6a6574ea
ene8255 <86288109+ene8255@users.noreply.github.com>
2022-03-02 23:19:09 +0900

    폴더 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/include/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
